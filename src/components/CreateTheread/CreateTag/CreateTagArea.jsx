cc075910fa8b7c1a2c2799f1a88f5bc9d54c5eac
tkknot <takeda19991015@gmail.com>
2023-09-11 22:36:47 +0900

    update: design

--- src/components/CreateTheread/CreateTag/CreateTagArea.jsx
+++ src/components/CreateTheread/CreateTag/CreateTagArea.jsx
@@ -2,21 +2,26 @@ import threads from "../../../data/threads.json";
 export const CreateTagArea = () => {
   //localstorageからタグ一覧をとってくる
   let tags = [];
+  let tagsColor = [];
 
   threads.map((thread) => {
     thread.tags.forEach((tag) => tags.push(tag));
+    tagsColor.push(thread.tagColor);
   });
 
-  // 重複排除
+  //重複排除
   let tags_set = new Set(tags);
+  let tagsColor_set = new Set(tagsColor);
 
-  // 排除後、配列に戻す
+  //排除後、配列に戻す
   tags = Array.from(tags_set);
+  tagsColor = Array.from(tagsColor_set);
 
   console.log(tags);
+  console.log(tagsColor);
 
   //カラーをランダムにする
-  const tagColors = [
+  const tagSetitingColor = [
     "bg-blue-100",
     "bg-green-100",
     "bg-red-100",
@@ -27,18 +32,14 @@ export const CreateTagArea = () => {
     "bg-orange-100",
   ];
 
-  const colorRandom = Math.floor(Math.random() * tagColors.length);
+  const colorRandom = Math.floor(Math.random() * tagSetitingColor.length);
 
   //TODO 新規でタグを作る
 
   return (
     <div className='flex flex-row space-x-2 mb-4'>
-      <span className='py-1 px-4 inline-flex rounded-full font-semibold bg-blue-100 text-blue-800'>
-        tag1 x
-      </span>
-      <span className='py-1 px-4 inline-flex rounded-full font-semibold bg-green-100 text-green-800'>
-        tag2 x
-      </span>
+      <span className='tag bg-blue-100'>tag1 x</span>
+      <span className='tag bg-green-100'>tag2 x</span>
       <button className='py-1 px-4 inline-flex rounded-full font-bold bg-gray-200 text-gray-500'>
         add tags | +
       </button>
